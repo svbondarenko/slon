3393739e4d9b124385911ed3259990a72beb2731
unknown <Yaroslav_Holub@EPUAKYIW0993.kyiv.epam.com>
2015-01-22 18:43:32 +0200

    init

---
+++ src/main/java/com/scm/rest/ScmPrototypeResource.java
@@ -0,0 +1,7 @@
+package com.scm.rest;
+
+/**
+ * Created by Serhii_Bondarenko on 1/22/2015.
+ */
+public class ScmPrototypeResource {
+}
---
+++ src/main/java/com/scm/rest/jersey/SCMApplication.java
@@ -0,0 +1,21 @@
+package com.scm.rest.jersey;
+
+import com.scm.rest.ScmPrototypeResource;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
+
+/**
+ * Created by Serhii_Bondarenko on 1/22/2015.
+ */
+public class SCMApplication  extends ResourceConfig {
+
+    /**
+     * Register JAX-RS application components.
+     */
+    public SCMApplication(){
+        register(RequestContextFilter.class);
+        register(ScmPrototypeResource.class);
+        register(JacksonFeature.class);
+    }
+}
